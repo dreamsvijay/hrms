8078408d7111b1b53f53341d7d0f0a8e9effd47d
Boominathan <boominathan@dreamguys.co.in>
2018-03-10 09:56:41 +0530

    Service added in angular and node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormsModule, } from '@angular/forms';
+import { CommonService } from './common.service';
+
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+
+  constructor(private newService: CommonService, ) { }
+  Repdata;
+  valbutton = "Save";
+
+
+  ngOnInit() {
+    this.newService.GetUser().subscribe(data => this.Repdata = data)
+  }
+
+  onSave = function (user, isValid: boolean) {
+    user.mode = this.valbutton;
+    this.newService.saveUser(user)
+      .subscribe(data => {
+        alert(data.data);
+
+        this.ngOnInit();
+      }
+        , error => this.errorMessage = error)
+
+  }
+  edit = function (kk) {
+    this.id = kk._id;
+    this.first_name = kk.first_name;
+    this.last_name = kk.last_name;
+    this.valbutton = "Update";
+  }
+
+  delete = function (id) {
+    this.newService.deleteUser(id)
+      .subscribe(data => { alert(data.data); this.ngOnInit(); }, error => this.errorMessage = error)
+  }
+
+} 
