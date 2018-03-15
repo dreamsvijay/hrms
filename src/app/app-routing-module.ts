698976b5a90e853cd2a728c6c7771302a7c9cbf4
Boominathan <boominathan@dreamguys.co.in>
2018-03-15 17:58:55 +0530

    mereged master files

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,13 +4,15 @@ import { AuthGuardService } from './auth-guard.service';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginInComponent } from './login-in/login-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/log-in', pathMatch: 'full' },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'log-in', component: LoginInComponent }, 
-    { path: 'home', canActivate: [ AuthGuardService ], component: DashboardComponent }  
+    { path: 'home', canActivate: [ AuthGuardService ], component: DashboardComponent },
+    { path: 'customer', canActivate: [ AuthGuardService ], component: CustomerComponent }  
 ];
 
 @NgModule({
---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+//Import the API for building a form
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: [
+  				'./customer.component.css',
+  				'../../assets/css/dataTables.bootstrap.min.css',
+  				'../../assets/css/select2.min.css',
+  				'../../assets/css/bootstrap-datetimepicker.min.css',
+  				'../../assets/css/style.css'
+  			]
+})
+export class CustomerComponent implements OnInit {
+	
+	customers;
+	customerForm: FormGroup;
+	
+	constructor( private apiService: ApiService, private router: Router ) { }
+
+	ngOnInit() {
+	   	this.loadScript('../../assets/js/jquery-3.2.1.min.js');
+		this.loadScript('../../assets/js/bootstrap.min.js');
+		this.loadScript('../../assets/js/jquery.dataTables.min.js');
+		this.loadScript('../../assets/js/dataTables.bootstrap.min.js');
+		this.loadScript('../../assets/js/select2.min.js');
+		this.loadScript('../../assets/js/moment.min.js');
+		this.loadScript('../../assets/js/bootstrap-datetimepicker.min.js');
+	  	this.loadScript('../../assets/js/app.js');
+	  	
+		this.customerForm = new FormGroup({
+			customer_number: new FormControl(),
+			company_name: new FormControl(),
+			company_number: new FormControl(),
+			gst_number: new FormControl(), 
+			title: new FormControl(),
+			first_name: new FormControl(),
+			last_name: new FormControl(),
+			email: new FormControl(),
+			phone: new FormControl(),
+			mobile_phone: new FormControl(),
+			invoice_address_line1: new FormControl(),
+			invoice_address_line2: new FormControl(),
+			invoice_postcode: new FormControl(),
+			invoice_city: new FormControl(),
+			invoice_country: new FormControl(),
+			invoice_language: new FormControl(),
+			payment_terms: new FormControl(),
+			vat: new FormControl()
+		});
+    
+		this.getCustomers();
+  	}
+ 
+	getCustomers = function() {
+		this.apiService.getCustomers()
+	      .subscribe(data => {
+	      	this.customers = data;	
+	      }, error => { console.log(error) });
+	}
+	
+	onLogout = function() {
+	      this.apiService.logout()
+	      .subscribe(data => {
+	      	if( data.id ) {
+	        	localStorage.removeItem(data.id);
+	        	localStorage.removeItem("HRMS_current_user");
+	        }
+	        this.router.navigate(['']);
+	      }, error => { console.log(error) });
+	      return false;
+ 	}
+
+	onFormSubmit = function(){
+	    this.apiService.createCustomer(this.customerForm.value)
+	      .subscribe(data => {
+	      	if( data ) {
+	      		this.router.navigate(['customer']);
+	      		this.getCustomers();
+	      	}
+	      	else {
+	      		this.router.navigate(['customer']);
+	      	}
+	      }, error => this.errorMessage = error);
+	    return false;
+	}
+	
+    onCancel = function() {
+    	this.customerForm.reset();
+    } 
+    
+	public loadScript(url) {
+        let body = <HTMLDivElement> document.body;
+        let script = document.createElement('script');
+        script.innerHTML = '';
+        script.src = url;
+        script.async = true;
+        script.defer = true;
+        body.appendChild(script);
+    }
+}
